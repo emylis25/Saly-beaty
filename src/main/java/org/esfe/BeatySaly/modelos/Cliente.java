d26f66665ee5f5d12067597c0a7edc6b30d00b84
Sarai Jemima Arenivar Hernandez <6796008@clases.edu.sv>
2025-07-29 10:07:38 -0600

    se creo la entidad Cliente

---
+++ src/main/java/org/esfe/BeatySaly/modelos/Cliente.java
@@ -0,0 +1,99 @@
+package org.esfe.BeatySaly.modelos;
+
+import java.util.Objects;
+import java.util.Set;
+
+public class Cliente {
+    private int id;
+    private String nombre;
+    private String correo;
+    private int telefono;
+    private String password;
+    private String rol;
+
+    public Cliente(){
+
+    }
+
+    public Cliente(int id, String nombre, String correo, int telefono, String password, String rol){
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.password = password;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    // --- equals(), hashCode(), toString() ---
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return id == cliente.id; // Asumimos que el ID es el identificador único
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", correo='" + correo + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", rol='" + rol + '\'' +
+                '}';
+    }
+}
