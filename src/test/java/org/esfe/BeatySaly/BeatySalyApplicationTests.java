c69eda374dcd6fc1e45f541939d74b3d65d8ec01
Emily Lisbeth Bolaño Flores <emilyflores.093@gmail.com>
2025-07-28 19:33:05 -0600

    Primer commit desde IntelliJ

---
+++ src/test/java/org/esfe/BeatySaly/BeatySalyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esfe.BeatySaly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatySalyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
