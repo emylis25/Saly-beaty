7db0e187fa6fc97f05e4f5ae2230a7a55f64c71b
Emily Lisbeth Bolaño Flores <emilyflores.093@gmail.com>
2025-08-15 09:58:42 -0600

    servicio interfaz de Cita

---
+++ src/main/java/org/esfe/BeatySaly/servicios/interfaces/ICitaService.java
@@ -0,0 +1,28 @@
+package org.esfe.BeatySaly.servicios.interfaces;
+
+import org.esfe.BeatySaly.modelos.Cita;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface ICitaService {
+
+        List<Cita> obtenerTodos();
+
+        Page<Cita> buscarTodosPaginados(Pageable pageable);
+
+        Cita buscarPorId(Integer id);
+
+        Cita crear(Cita cita);
+
+        Cita editar(Cita cita);
+
+        void eliminarPorId(Integer id);
+
+        Page<Cita> buscarPorNombreTrabajadorYCliente(
+                String nombreTrabajador,
+                String nombreCliente,
+                Pageable pageable
+        );
+    }
