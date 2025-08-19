5dbaad77be76214f449d206903b684583f213201
Emily Lisbeth Bolaño Flores <emilyflores.093@gmail.com>
2025-08-19 11:20:59 -0600

    prueba

--- src/main/java/org/esfe/BeatySaly/servicios/implementaciones/AdministradorService.java
+++ src/main/java/org/esfe/BeatySaly/servicios/implementaciones/AdministradorService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+//prueba
 @Service
 public class AdministradorService  implements IAdministradorService {
 
