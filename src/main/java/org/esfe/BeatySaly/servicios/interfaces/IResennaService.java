9f0591d1109fa656c48ad9852361543a53b94ac3
Emily Lisbeth Bolaño Flores <emilyflores.093@gmail.com>
2025-08-19 08:23:34 -0600

    arreglo del nombre Resenna

--- src/main/java/org/esfe/BeatySaly/servicios/interfaces/IResennaService.java
+++ src/main/java/org/esfe/BeatySaly/servicios/interfaces/IResennaService.java
@@ -5,7 +5,7 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-public interface IResnnaService {
+public interface IResennaService {
 
     List<Resenna> obtenerTodos();
 
