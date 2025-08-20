18673dd6ee5b8c571aa2728efba4db6684c454c7
Sarai Jemima Arenivar Hernandez <6796008@clases.edu.sv>
2025-08-20 15:44:04 -0600

    Se creo el controlador de Trabajador

---
+++ src/main/java/org/esfe/BeatySaly/controladores/TrabajadorController.java
@@ -0,0 +1,106 @@
+package org.esfe.BeatySaly.controladores;
+
+import org.esfe.BeatySaly.modelos.Trabajador;
+import org.esfe.BeatySaly.servicios.interfaces.ITrabajadorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@RequestMapping("/trabajadores")
+public class TrabajadorController {
+
+    @Autowired
+    private ITrabajadorService trabajadorService;
+
+    @GetMapping
+    public String index(Model model, @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size){
+        int currentPage = page.orElse(1) - 1;
+        int pageSize = size.orElse(5);
+        Pageable pageable = PageRequest.of(currentPage, pageSize);
+
+        Page<Trabajador> trabajadores = trabajadorService.buscarTodosPaginados(pageable);
+        model.addAttribute("trabajadores", trabajadores);
+
+        int totalPages = trabajadores.getTotalPages();
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+
+        return "trabajador/index";
+    }
+
+    @GetMapping("/create")
+    public String create(Trabajador trabajador){
+        return "trabajador/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Trabajador trabajador, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            attributes.addFlashAttribute("error", "No se pudo guardar el trabajador debido a un error.");
+            return "trabajador/create";
+        }
+
+        if (trabajador.getId() == 0) {
+            trabajadorService.crear(trabajador);
+            attributes.addFlashAttribute("msg", "Trabajador creado correctamente.");
+        } else {
+            trabajadorService.actualizar(trabajador);
+            attributes.addFlashAttribute("msg", "Trabajador editado correctamente.");
+        }
+
+        return "redirect:/trabajadores";
+    }
+
+    @GetMapping("/details/{id}")
+    public String details(@PathVariable("id") int id, Model model){
+        Trabajador trabajador = trabajadorService.obtenerPorId(id);
+        if (trabajador == null) {
+            return "redirect:/trabajadores";
+        }
+        model.addAttribute("trabajador", trabajador);
+        return "trabajador/details";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") int id, Model model){
+        Trabajador trabajador = trabajadorService.obtenerPorId(id);
+        if (trabajador == null) {
+            return "redirect:/trabajadores";
+        }
+        model.addAttribute("trabajador", trabajador);
+        return "trabajador/edit";
+    }
+
+    @GetMapping("/remove/{id}")
+    public String remove(@PathVariable("id") int id, Model model){
+        Trabajador trabajador = trabajadorService.obtenerPorId(id);
+        if (trabajador == null) {
+            return "redirect:/trabajadores";
+        }
+        model.addAttribute("trabajador", trabajador);
+        return "trabajador/delete";
+    }
+
+    @PostMapping("/delete")
+    public String delete(Trabajador trabajador, RedirectAttributes attributes){
+        trabajadorService.eliminar(trabajador.getId());
+        attributes.addFlashAttribute("msg", "Trabajador eliminado correctamente.");
+        return "redirect:/trabajadores";
+    }
+}
