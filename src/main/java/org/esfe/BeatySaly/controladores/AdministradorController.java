2c85af09034901fa4a3e907297d7c9a831f7b410
Emily Lisbeth Bolaño Flores <emilyflores.093@gmail.com>
2025-08-26 16:48:33 -0600

    cambios en controladores

--- src/main/java/org/esfe/BeatySaly/controladores/AdministradorController.java
+++ src/main/java/org/esfe/BeatySaly/controladores/AdministradorController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/administradores")
+@RequestMapping("/administradores")
 public class AdministradorController {
 
     @Autowired
--- src/main/java/org/esfe/BeatySaly/controladores/ServicioController.java
+++ src/main/java/org/esfe/BeatySaly/controladores/ServicioController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/servicios")
+@RequestMapping("/servicios")
 public class ServicioController {
 
     @Autowired
