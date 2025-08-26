aff48dd6172aa59ede9507ed2dc531380926d94e
Emily Lisbeth Bolaño Flores <emilyflores.093@gmail.com>
2025-08-26 10:55:56 -0600

    actualizacion

--- src/main/java/org/esfe/BeatySaly/servicios/interfaces/IAuthService.java
+++
@@ -1,6 +0,0 @@
-package org.esfe.BeatySaly.servicios.interfaces;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface IAuthService extends UserDetailsService {
-}
