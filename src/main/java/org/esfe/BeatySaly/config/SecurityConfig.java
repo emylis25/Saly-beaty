c89d240089c8dd4b2b1615745fa91af74c7d8eee
Emily Lisbeth Bolaño Flores <emilyflores.093@gmail.com>
2025-08-27 15:34:26 -0600

    Cerrar Session funcionando

--- src/main/java/org/esfe/BeatySaly/config/SecurityConfig.java
+++ src/main/java/org/esfe/BeatySaly/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login?logout")
+                        .logoutSuccessUrl("/")
                         .permitAll()
                 );
 
